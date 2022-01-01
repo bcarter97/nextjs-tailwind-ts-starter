9a2df144924df7d9b1ae434cf9a4506d19a12d07
Ben Carter <benjohn.carter@gmail.com>
2022-01-01 21:11:51 +0000

    feat: use button in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
+import { Button } from "components";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>Home</div>;
+  return (
+    <div className="p-12">
+      <p className="py-6">Home</p>
+      <Button>Me button</Button>
+    </div>
+  );
 };
 
 export default Home;
